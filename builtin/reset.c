4b7ec96ca9db6a5ed853aba0ec212e322b03febd
brian m. carlson <sandals@crustytoothpaste.net>
2017-05-20 21:00:59 +0000

    Convert find_unique_abbrev* to struct object_id
    Convert find_unique_abbrev and find_unique_abbrev_r to each take a
    pointer to struct object_id.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- builtin/reset.c
+++ builtin/reset.c
@@ -109,7 +109,7 @@ static void print_new_head_line(struct commit *commit)
 	const char *hex, *body;
 	const char *msg;
 
-	hex = find_unique_abbrev(commit->object.oid.hash, DEFAULT_ABBREV);
+	hex = find_unique_abbrev(&commit->object.oid, DEFAULT_ABBREV);
 	printf(_("HEAD is now at %s"), hex);
 	msg = logmsg_reencode(commit, NULL, get_log_output_encoding());
 	body = strstr(msg, "\n\n");
