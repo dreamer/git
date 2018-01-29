76f0b5f159a8f0cfeaa6d953c91d5e4473692a5a
Patryk Obara <patryk.obara@gmail.com>
2018-01-29 22:30:10 +0100

    WIP sha1_file: use the_hash_algo rawsz for fill_path

--- sha1_file.c
+++ sha1_file.c
@@ -312,7 +312,7 @@ out:
 static void fill_sha1_path(struct strbuf *buf, const unsigned char *sha1)
 {
 	int i;
-	for (i = 0; i < 20; i++) {
+	for (i = 0; i < the_hash_algo->rawsz; i++) {
 		static char hex[] = "0123456789abcdef";
 		unsigned int val = sha1[i];
 		strbuf_addch(buf, hex[val >> 4]);
